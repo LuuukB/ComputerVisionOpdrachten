033a0a3eeb54627d3ba6246f245e58b73984a0ae
Luuk <luukboerman@hotmail.com>
2025-05-09 11:56:44 +0200

    added chapter1 and 2

---
+++ VisionPractica/Practicum1/Chapter3.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+
+///////////////  Resize and Crop //////////////////////
+
+void main() {
+
+	std::string path = "Resources/test.png";
+	cv::Mat img = cv::imread(path);
+	cv::Mat imgResize, imgCrop;
+
+	//cout << img.size() << endl;
+	resize(img, imgResize, cv::Size(), 1.5, 1.5);
+
+	cv::Rect roi(0, 150, 200, 200);
+	imgCrop = img(roi);
+
+	imshow("Image", img);
+	imshow("Image Resize", imgResize);
+	imshow("Image Crop", imgCrop);
+	cv::waitKey(0);
+}
+
+
