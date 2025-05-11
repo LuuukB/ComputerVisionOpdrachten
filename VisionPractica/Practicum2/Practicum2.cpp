6cc1c44d2f9405d8c0cd4d597bc88107ebc3c509
Luuk <luukboerman@hotmail.com>
2025-05-11 12:54:28 +0200

    added opdracht2.3-C

---
+++ VisionPractica/Practicum2/Practicum2.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+
+int main()
+{
+	std::string path = "Resources/bars.bmp";
+	cv::Mat img = cv::imread(path);
+	cv::Mat imgOpening, imgBlur, imgInv;
+
+	cv::bitwise_not(img, imgInv);
+
+	cv::GaussianBlur(imgInv, imgBlur, cv::Size(7, 7), 5, 0);
+
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(10, 10));
+	cv::morphologyEx(img, imgOpening, cv::MORPH_OPEN, kernel);
+	cv::imshow("Image", img);
+	cv::imshow("Image Opening", imgOpening);
+	cv::waitKey(0);
+
+
+}
